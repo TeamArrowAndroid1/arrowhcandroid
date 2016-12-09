72c761ef419e8e70b3550df3f351bf49711c42c4
Luis Esquivel <laesquivel@hotmail.com>
2016-12-09 17:39:21 -0500

    Adding Test List and Add Test Activity

--- arrowapp/src/main/java/com/arrow/arrowhc/AddTest.java
+++ arrowapp/src/main/java/com/arrow/arrowhc/AddTest.java
@@ -1,7 +1,30 @@
 package com.arrow.arrowhc;
 
+import android.content.Intent;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.support.v7.widget.ToolbarWidgetWrapper;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.w3c.dom.Text;
+
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 public class AddTest extends AppCompatActivity {
 
@@ -9,5 +32,84 @@ public class AddTest extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_test);
+
+        ActionBar ab = getSupportActionBar();
+        ab.setDefaultDisplayHomeAsUpEnabled(true);
+
+        TextView tvName =(TextView)findViewById(R.id.tvName);
+        final TextView tvResponse = (TextView) findViewById(R.id.textView2);
+
+        final String sPatientName = getIntent().getStringExtra("name");
+        final String sPatienId = getIntent().getStringExtra("id");
+        if(sPatientName!=null)
+        {
+            tvName.setText(sPatientName);
+        }
+
+        Button saveButton = (Button) findViewById(R.id.saveButton);
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                /*
+                Intent intent=new Intent(getBaseContext(),AddTest.class);
+                intent.putExtra("id",sPatienId);
+                startActivity(intent);
+                */
+
+                EditText etCholesterol = (EditText) findViewById(R.id.etCholesterol);
+                EditText etTemperature = (EditText) findViewById(R.id.etTemperature);
+                EditText etBlood = (EditText) findViewById(R.id.etBlood);
+                EditText etHeart = (EditText) findViewById(R.id.etHeart);
+
+                final String sCholesterol = etCholesterol.getText().toString();
+                final String sTemperature = etTemperature.getText().toString();
+                final String sBlood = etBlood.getText().toString();
+                final String sHeart = etHeart.getText().toString();
+
+                final String currentDateTimeString = DateFormat.getDateTimeInstance().format(new Date());
+
+                //Toast.makeText(this,"Put"+ paswdP+","+finalDid+","+ doctorName, Toast.LENGTH_LONG).show();
+
+                RequestQueue mRequestQueue = Volley.newRequestQueue(getBaseContext());
+
+                String url = "https://arrowhc.herokuapp.com/test";
+
+                tvResponse.setText(url);
+
+                StringRequest MyStringRequest = new StringRequest(Request.Method.POST, url, new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+
+                        tvResponse.setText(response);
+                        //This code is executed if the server responds, whether or not the response contains data.
+                        //The String 'response' contains the server's response.
+                    }
+                }, new Response.ErrorListener() { //Create an error listener to handle errors appropriately.
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        //This code is executed if there is an error.
+                    }
+                }) {
+                    protected Map<String, String> getParams() {
+                        Map<String, String> MyData = new HashMap<String, String>();
+                        MyData.put("patient_name",sPatientName); //Add the data you'd like to send to the server.
+                        MyData.put("patient_id", sPatienId);
+                        MyData.put("blood_presure", sBlood);
+                        MyData.put("cholesterol", sCholesterol);
+                        MyData.put("heart_rate", sHeart);
+                        MyData.put("temperature", sTemperature);
+                        MyData.put("date", currentDateTimeString);
+
+                        return MyData;
+                    }
+                };
+
+                mRequestQueue.add(MyStringRequest);
+
+
+            }
+        });
+
     }
 }
--- arrowapp/src/main/java/com/arrow/arrowhc/PatientPage.java
+++ arrowapp/src/main/java/com/arrow/arrowhc/PatientPage.java
@@ -3,23 +3,57 @@ package com.arrow.arrowhc;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class PatientPage extends AppCompatActivity {
 
-    Button add,view,delete,test;View v; String id;
+    TextView tv;ListView lv;
+    ArrayList<HashMap<String,String>> arrayList;
+    RequestQueue requestQueue;
+
+    Button add,view,delete,test;View v;
+    String id, patientName;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_patient_page);
+
+        requestQueue= Volley.newRequestQueue(getBaseContext());
+        arrayList=new ArrayList<>();
+        lv=(ListView)findViewById(R.id.list);
+
         id=getIntent().getStringExtra("id");
+        patientName=getIntent().getStringExtra("name");
+
         //Toast.makeText(getBaseContext(), id, Toast.LENGTH_SHORT).show();
+        tv = (TextView) findViewById(R.id.textView);
+        tv.setText(id);
+
         add=(Button)findViewById(R.id.add);
         view=(Button)findViewById(R.id.viewp);
         delete=(Button)findViewById(R.id.deletep);
 
-        test=(Button)findViewById(R.id.buttonTest);
+        test=(Button)findViewById(R.id.addTest);
 
         //if there is no paitent id
            view.setVisibility(v.GONE);
@@ -38,10 +72,72 @@ public class PatientPage extends AppCompatActivity {
             public void onClick(View v) {
                 Intent intent=new Intent(getBaseContext(),AddTest.class);
                 intent.putExtra("id",id);
+                intent.putExtra("name",patientName);
                 startActivity(intent);
             }
         });
 
+        data();
 
     }
+
+    public void data()
+    {
+        String  rurl="https://arrowhc.herokuapp.com/test/"+id;
+
+        JsonArrayRequest req = new JsonArrayRequest(rurl,
+                new Response.Listener<JSONArray>() {
+                    @Override
+                    public void onResponse(JSONArray response) {
+                        //String da="";
+
+                        //  Toast.makeText(StaffActivity.this, url, Toast.LENGTH_SHORT).show();
+                        try {
+                            for (int i = 0; i <response.length(); i++) {
+
+                                JSONObject jresponse = response.getJSONObject(i);
+
+                                String blood = jresponse.getString("blood_presure");
+                                String cholesterol= jresponse.getString("cholesterol");
+                                String heart = jresponse.getString("heart_rate");
+                                String temperature = jresponse.getString("temperature");
+                                String testdate = jresponse.getString("date");
+                                Toast.makeText(PatientPage.this,testdate, Toast.LENGTH_SHORT).show();
+                                HashMap<String,String> contact = new HashMap<>();
+
+                                contact.put("date",testdate);
+                                contact.put("blood",blood);
+                                contact.put("cholesterol",cholesterol);
+                                contact.put("heart",heart);
+                                contact.put("temperature",temperature);
+
+                                arrayList.add(contact);
+                                ListAdapter adapter = new SimpleAdapter(
+                                        PatientPage.this, arrayList, R.layout.list_test, new String[]{"date","blood",
+                                        "cholesterol","heart","temperature"}, new int[]{R.id.date,R.id.blood,
+                                        R.id.cholesterol,R.id.heart,R.id.temperature});
+
+                                lv.setAdapter(adapter);
+
+                            }
+
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+
+
+                    }
+
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.e("Volley", "Error");
+            }
+        }
+        );
+        requestQueue.add(req);
+
+
+    }
+
 }
